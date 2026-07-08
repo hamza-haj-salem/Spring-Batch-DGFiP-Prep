b00741f2a280c2c818fd8d6aba7b8c895315b99f
hamza-haj-salem <hajsalemhamza@gmail.com>
2026-07-08 11:35:29 +0200

    test 5 chekks master

--- src/test/java/springBatch/batch/processor/GirItemProcessorTest.java
+++ src/test/java/springBatch/batch/processor/GirItemProcessorTest.java
@@ -15,7 +15,7 @@ public class GirItemProcessorTest {
     @Test
     void shouldProcessValidGirXml() throws Exception {
 
-        //test cheks jenkins -> Github depuis master 4
+        //test cheks jenkins -> Github depuis master 5
         // GIVEN : création du MessageSpec
         MessageSpecDto messageSpec = new MessageSpecDto();
         messageSpec.setMessageRefId("MSG001");
